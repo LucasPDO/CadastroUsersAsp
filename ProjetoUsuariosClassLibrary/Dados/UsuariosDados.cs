7601d4e7a7c3de7904cca009c7b2582c920fd4fc
Lucas Piccoli <lucaspiccoli70@gmail.com>
2024-04-23 11:52:04 -0300

    projeto asp net cadastro usuario

---
+++ ProjetoUsuariosClassLibrary/Dados/UsuariosDados.cs
@@ -0,0 +1,25 @@
+﻿using AcessoDadosClassLibrary;
+using ProjetoUsuariosClassLibrary.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoUsuariosClassLibrary.Dados
+{
+    public class UsuariosDados
+    {
+        internal void CreateUsuario(Usuarios usuario)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "procedure aqui";
+            command.Parameters.Add(DAO.RetornaDbParameter(@usuario.Nome, usuario.Nome, DbType.String));
+            command.Parameters.Add(DAO.RetornaDbParameter(@usuario.Email, usuario.Email, DbType.String));
+            DAO.ExecutaProcedure(command);
+        }
+
+    }
+}
---
+++ ProjetoUsuariosClassLibrary/Negocios/UsuariosNegocios.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoUsuariosClassLibrary.Dados;
+using ProjetoUsuariosClassLibrary.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoUsuariosClassLibrary.Negocios
+{
+    public class UsuarioNegocios
+    {
+        public void CreateUsuario(Usuarios usuario)
+        {
+            //Colocar as regras de negocio
+            if (String.IsNullOrWhiteSpace(usuario.Nome))
+            {
+                throw new ValidationException("Favor Inserir um Nome");
+            }
+            if (String.IsNullOrWhiteSpace(usuario.Email))
+            {
+                throw new ValidationException("Favor Inserir um Email");
+            }
+
+            new UsuariosDados().CreateUsuario(usuario);
+        }
+    }
+}
